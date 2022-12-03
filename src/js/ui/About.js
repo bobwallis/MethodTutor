21e3db62c0b4a950a2a743dff163a5c2be2579e2
Bob Wallis <bob.wallis@gmail.com>
2022-12-03 11:07:13 +0000

    Close the info when pressing Esc

--- src/js/ui/About.js
+++ src/js/ui/About.js
@@ -1,28 +1,34 @@
 define( ['ready'], function( ready ) {
     ready( function() {
-        // Create overlay
+        // Create overlay element
         var overlay = document.createElement( 'div' );
         overlay.id = 'overlay';
         overlay.className = 'hide';
 
-        // Find about element
+        // Find the about element
         var about = document.getElementById( 'about' );
 
-        // Add overlay to document
+        // Add the overlay element to the document
         document.body.insertBefore( overlay, about );
 
-        // Handle click to open/close the info window
-        document.getElementById( 'aboutButton' ).addEventListener( 'click', function() {
+        // Functions to open and close the about box
+        var openAbout = function() {
             overlay.className = 'active';
             about.className   = 'active';
-        } );
-        overlay.addEventListener( 'click', function() {
+        };
+        var closeAbout = function() {
             overlay.className = 'hide';
             about.className   = 'hide';
-        } );
-        about.addEventListener( 'click', function() {
-            overlay.className = 'hide';
-            about.className   = 'hide';
-        } );
+        };
+
+        // Add event listeners
+        document.getElementById( 'aboutButton' ).addEventListener( 'click', openAbout );
+        overlay.addEventListener( 'click', closeAbout );
+        about.addEventListener( 'click', closeAbout );
+        document.body.addEventListener( 'keydown', function( e ) {
+            if( e.key === 'Escape' ) {
+                closeAbout();
+            }
+		} );
     } );
 } );
