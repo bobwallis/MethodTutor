f09d6c97fa4cb4827e655c2281317a9ae6ea5733
Bob Wallis <bob.wallis@gmail.com>
2025-03-09 09:55:20 +0000

    Make about box close properly

--- src/js/ui/About.js
+++ src/js/ui/About.js
@@ -1,30 +1,38 @@
 define( ['ready'], function( ready ) {
     ready( function() {
+        // Find the about element
+        var about = document.getElementById( 'about' );
+
         // Create overlay element
         var overlay = document.createElement( 'div' );
         overlay.id = 'overlay';
         overlay.className = 'hide';
-
-        // Find the about element
-        var about = document.getElementById( 'about' );
-
-        // Add the overlay element to the document
         document.body.insertBefore( overlay, about );
 
         // Functions to open and close the about box
+        var aboutOpen = false;
+        var toggleAbout = function() {
+            aboutOpen? closeAbout() : openAbout();
+        };
         var openAbout = function() {
             overlay.className = 'active';
             about.className   = 'active';
+            aboutOpen = true;
         };
         var closeAbout = function() {
             overlay.className = 'hide';
             about.className   = 'hide';
-        };
+            aboutOpen = false;
+        }
 
         // Add event listeners
-        document.getElementById( 'aboutButton' ).addEventListener( 'click', openAbout );
-        overlay.addEventListener( 'click', closeAbout );
-        about.addEventListener( 'click', closeAbout );
+        document.getElementById( 'aboutButton' ).addEventListener( 'click', toggleAbout );
+        overlay.addEventListener( 'click', toggleAbout );
+        document.getElementById( 'head' ).addEventListener( 'click', function( e ) {
+            if( !e.target.matches( '#aboutButton' ) ) {
+                closeAbout();
+            }
+        } );
         document.body.addEventListener( 'keydown', function( e ) {
             if( e.key === 'Escape' ) {
                 closeAbout();
